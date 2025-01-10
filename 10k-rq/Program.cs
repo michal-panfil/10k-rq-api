9a4f1f5a2d0a8414766e31172a678b5145f3c783
Michał Panfil <it.panfil@gmail.com>
2025-01-10 14:40:09 +0100

    init

---
+++ 10k-rq/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.MapPost("/bookEvent", (BookingRequest request) =>
+{
+    return ($"book {request.EventId} seat {request.SeatNumber}, for customer {request.CustomerId}");
+}).WithName("BookEvent")
+.WithOpenApi();
+
+app.Run();
+
+internal record BookingRequest(int SeatNumber, int EventId, int CustomerId);
