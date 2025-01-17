d7e947be81068c77617b4f30622d4b22872b3270
Michał Panfil <it.panfil@gmail.com>
2025-01-17 23:12:31 +0100

    fb to daatabase

--- 10k-rq/Db/PrepareDb.sql
+++ 10k-rq/Db/PrepareDb.sql
@@ -2,16 +2,30 @@ CREATE DATABASE EventBooker;
 
 USE EventBooker;
 
-CREATE TABLE SiteEvents (
-    Id INT NOT NULL PRIMARY KEY, 
+CREATE TABLE Venue (
+    Id SERIAL PRIMARY KEY, 
     Name VARCHAR(255) NOT NULL,        
-    Venue VARCHAR(255) NOT NULL,       
-    Date DATETIME NOT NULL             
+    SeatsCount INT NOT NULL             
 );
+
+CREATE TABLE SiteEvents (
+    Id SERIAL PRIMARY KEY, 
+    Name VARCHAR(255) NOT NULL,        
+    VenueId INT NOT NULL,       
+    EventStartDate DATE NOT NULL,   
+    FOREIGN KEY (VenueId) REFERENCES Venue(Id)
+    )
 
 CREATE TABLE  BookingRequest(
     Id INT NOT NULL PRIMARY KEY, 
     SeatNumber INT NOT NULL,
     EventId INT NOT NULL,
-    CustomerId INT NOT NULL
+    CustomerId INT NOT NULL,
+    FOREIGN KEY (EventId) REFERENCES SiteEvents(Id)
 )
+
+INSERT INTO  Venue (Name, SeatsCount)
+VALUES ('Boston_Stadion', 100000), ('NewYork_Stadion', 100000)
+
+INSERT INTO SiteEvents (Name, VenueId, EventStartDate)
+VALUES ('Concert1', 1, '2025-01-17')
